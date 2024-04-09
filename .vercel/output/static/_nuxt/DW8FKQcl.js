d32bd5c96f4d419edad47b95cb16f3a7eea28057
seoro-malgm <seoromalgm@gmail.com>
2024-04-10 00:22:19 +0900

    DEPLOY

---
+++ .vercel/output/static/_nuxt/DW8FKQcl.js
@@ -0,0 +1,1 @@
+import{c as e,o as t}from"./BMtV3dYn.js";const s={__name:"index",props:{data:{type:String,default:null}},setup(a){return(n,r)=>(t(),e("div",null,"this is faq 관리"))}};export{s as default};
