fb33acb47030876c15ccad3256c35798c030ebb7
seoro-malgm <seoromalgm@gmail.com>
2024-09-14 21:39:18 +0900

    UPDATE 연사정보 등

---
+++ components/table/timeline.vue
@@ -0,0 +1,221 @@
+<template>
+  <div class="container overflow-x-auto">
+    <table class="time-table">
+      <thead>
+        <tr>
+          <th width="15%">시간</th>
+          <th width="75%">주제</th>
+          <th width="10%">비고</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="time">10:00 - 10:30</td>
+          <td>{{ $t("program_timetable__desc01") }}</td>
+          <td class="text-center">30 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td class="time">10:30 - 10:35</td>
+          <td>{{ $t("program_timetable__desc02") }}</td>
+          <td class="text-center">5 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td class="time">10:35 - 10:40</td>
+          <td>{{ $t("program_timetable__desc03") }}</td>
+          <td class="text-center">5 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td class="time">10:40 - 10:45</td>
+          <td>
+            {{ $t("program_timetable__desc04") }}
+          </td>
+          <td class="text-center">5 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td class="time">10:45 - 10:55</td>
+          <td>{{ $t("program_timetable__desc05") }}</td>
+          <td class="text-center">10 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td class="time">10:55 - 11:05</td>
+          <td>{{ $t("program_timetable__desc06") }}</td>
+          <td class="text-center">10 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td class="time">11:10 - 11:20</td>
+          <td>{{ $t("program_timetable__desc07") }}</td>
+          <td class="text-center">10 {{ $t("word__time_min") }}</td>
+        </tr>
+
+        <tr>
+          <td rowspan="6" class="time">11:20 - 12:50</td>
+        </tr>
+        <tr>
+          <td colspan="2" class="subject">
+            {{ $t("program_timetable__desc08") }}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <pre>{{ getItem(items[0].id) }}</pre>
+          </td>
+          <td class="text-center">30 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td>
+            <pre>{{ getItem(items[1].id) }}</pre>
+          </td>
+          <td class="text-center">30 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td>{{ $t("program_timetable__desc09") }}</td>
+          <td class="text-center">20 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td>{{ $t("program_timetable__desc10") }}</td>
+          <td class="text-center">10 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td class="time">12:50 - 14:00</td>
+          <td>{{ $t("program_timetable__desc11") }}</td>
+          <td class="text-center">70 {{ $t("word__time_min") }}</td>
+        </tr>
+
+        <tr>
+          <td rowspan="6" class="time">14:00 - 15:30</td>
+        </tr>
+        <tr>
+          <td colspan="2" class="subject">
+            {{ $t("program_timetable__desc12") }}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <pre>{{ getItem(items[2].id) }}</pre>
+          </td>
+          <td class="text-center">30 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td>
+            <pre>{{ getItem(items[3].id) }}</pre>
+          </td>
+          <td class="text-center">30 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td>{{ $t("program_timetable__desc13") }}</td>
+          <td class="text-center">20 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td>{{ $t("program_timetable__desc14") }}</td>
+          <td class="text-center">10 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td rowspan="6" class="time">15:30 - 17:00</td>
+        </tr>
+        <tr>
+          <td colspan="2" class="subject">
+            {{ $t("program_timetable__desc15") }}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <pre>{{ getItem(items[4].id) }}</pre>
+          </td>
+          <td class="text-center">30 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td>
+            <pre>{{ getItem(items[5].id) }}</pre>
+          </td>
+          <td class="text-center">30 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td>{{ $t("program_timetable__desc16") }}</td>
+          <td class="text-center">20 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td>{{ $t("program_timetable__desc17") }}</td>
+          <td class="text-center">10 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td rowspan="5" class="time">17:00 - 18:30</td>
+        </tr>
+        <tr>
+          <td colspan="2" class="subject">
+            {{ $t("program_timetable__desc18") }}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <pre>{{ getItem(items[6].id) }}</pre>
+          </td>
+
+          <td class="text-center">30 {{ $t("word__time_min") }}</td>
+        </tr>
+
+        <tr>
+          <td>
+            <pre>{{ getItem(items[7].id) }}</pre>
+          </td>
+
+          <td class="text-center">30 {{ $t("word__time_min") }}</td>
+        </tr>
+        <tr>
+          <td>{{ $t("program_timetable__desc19") }}</td>
+          <td class="text-center">30 {{ $t("word__time_min") }}</td>
+        </tr>
+
+        <tr>
+          <td class="time">18:30 - 20:00</td>
+          <td colspan="2">{{ $t("program_timetable__desc20") }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+import spkeakerList from "@/data/speakerList.json";
+const items = computed(() => {
+  return spkeakerList;
+});
+
+const emit = defineEmits();
+
+const { t } = useI18n();
+const getItem = id => {
+  return `[${t(`${id}_title`)}]\n${t(`${id}_name`)}(${t(`${id}_name_en`)})\n${t(
+    `${id}_company`
+  )}  ${t(`${id}_position`)}`;
+};
+</script>
+
+<style lang="postcss" scoped>
+.time-table {
+  @apply max-w-full overflow-x-auto;
+  table {
+    @apply w-full border-collapse;
+    th,
+    td {
+      @apply border border-gray-300 p-2 text-sm;
+      &.time {
+        @apply font-bold text-center align-middle;
+        vertical-align: top;
+      }
+      &.subject {
+        @apply bg-grayscale-900 text-white text-center text-lg  py-2;
+      }
+      &.note {
+        @apply text-center;
+      }
+    }
+    th {
+      @apply bg-gray-800 text-white font-bold;
+    }
+
+    .title-row td {
+      @apply bg-gray-700 text-white font-bold text-center py-3;
+    }
+  }
+}
+</style>
